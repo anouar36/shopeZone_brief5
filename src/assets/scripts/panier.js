7d29f09b4e256fc97833932cd48756f83884ab09
naamyfatimazahra <naamyfatimazahra@gmail.com>
2024-11-12 10:56:17 +0100

    modifier l'ocale starage

--- src/assets/scripts/panier.js
+++ src/assets/scripts/panier.js
@@ -1,44 +1,13 @@
-const counterPanier = document.getElementById("counterPanier");
-const productList = document.getElementById("product-List");
-let count = 0;
-let products ;
-let produit;
-let panier;
-// Charger les produits depuis le fichier JSON
-fetch("../../file.json")
-  .then((response) => response.json()) // Convertir la réponse en JSON
-  .then((data) => {
-    products = data.products; // Assigner les produits à la variable Produits    
-  })
-  .catch((error) => {
-    console.error("Erreur lors de la récupération du fichier JSON:", error);
-  });
 
-// Fonction pour ajouter un produit au panier
-function addProduct(productId) {
-  panier = JSON.parse(localStorage.getItem("panier")) || [];
-  localStorage.getItem("count");
-  const produit = products.filter((el) => el.id === productId)[0]; 
-  if (produit) {
-    const produitExist = panier.find((item) => item.id === produit.id);
-    if (produitExist) {
-      console.log(produitExist);
-    } else {
-      panier.push({...produit });
-      count++;
-      counterPanier.textContent = count;
-    }
-    localStorage.setItem("panier", JSON.stringify(panier));
-  
-    
-  }
-}
-panier = JSON.parse(localStorage.getItem("panier"));
+const productList = document.getElementById("product-list");
 
-if (!panier) {
-  productList.innerHTML="no pruducts added to the cart!"
+let localPanier = JSON.parse(localStorage.getItem("panier"));
+let localCounter = JSON.parse(localStorage.getItem("count"));
+
+if (!localPanier) {
+  productList.innerHTML = "no pruducts added to the cart!";
 } else {
-  panier.forEach((el) => {
+  localPanier.forEach((el) => {
     let productElement = document.createElement("div");
     productElement.setAttribute(
       "class",
@@ -55,11 +24,13 @@ if (!panier) {
             }</p>
             <div class="flex items-center justify-between mt-4">
             <span class="text-lg font-semibold text-gray-800">€${
-              el.price
+              el.new_price
             }</span>
               <div class="flex items-center space-x-4">
                 <input type="number" value="1" min="1" class="w-16 h-10 text-center border rounded-md text-gray-800" />
-                <button class="text-red-500 hover:text-red-700 font-semibold" onclick="removeProduct(event)">Supprimer</button>
+                <button class="text-red-500 hover:text-red-700 font-semibold" onclick="removeProduct(${
+                  el.id
+                } )">Supprimer</button>
               </div>
             </div>
             </div>
@@ -70,14 +41,3 @@ if (!panier) {
 
 
 
-
-// // Fonction pour supprimer un produit du panier
-// function removeProduct(event) {
-//   const productElement = event.target.closest(".bg-white");
-//   if (productElement) {
-//     productElement.remove();
-//     count--;
-//     counterPanier.textContent = count;
-
-//   }
-// }
